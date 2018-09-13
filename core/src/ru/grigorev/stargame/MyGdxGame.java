ab4ae36c188f7198757793a7bd80fe995d5b35c5
meltiser <dmitriy-95@yandex.ru>
2018-09-13 21:32:40 +0300

    lesson1 - created background

--- core/src/ru/grigorev/stargame/MyGdxGame.java
+++ core/src/ru/grigorev/stargame/MyGdxGame.java
@@ -4,30 +4,34 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture backgroundImg;
+	Texture backgroundTexture;
+	Sprite backgroundSprite;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		backgroundImg = new Texture("background.jpg");
+		backgroundTexture = new Texture("background2048.jpg");
+		backgroundSprite = new Sprite(backgroundTexture);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(backgroundImg,0,0);
+		backgroundSprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		backgroundSprite.draw(batch);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		backgroundImg.dispose();
+		backgroundTexture.dispose();
 	}
 }
