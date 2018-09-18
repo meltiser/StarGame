a877a00f7f32a5d3ee71d701cf44b5d40e48d847
meltiser <dmitriy-95@yandex.ru>
2018-09-18 21:47:21 +0300

    lesson2

--- desktop/src/ru/grigorev/stargame/desktop/DesktopLauncher.java
+++ desktop/src/ru/grigorev/stargame/desktop/DesktopLauncher.java
@@ -2,11 +2,12 @@ package ru.grigorev.stargame.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import ru.grigorev.stargame.MyGdxGame;
+
+import ru.grigorev.stargame.StarGameMain;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new StarGameMain(), config);
+    }
 }
